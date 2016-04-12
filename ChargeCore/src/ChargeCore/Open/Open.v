ed41f2d2e9f579f4e3ab50de8ab2b380764b10bf
jesper-bengtson <jebe@itu.dk>
2016-04-12 15:52:22 +0200

    Fixed Open.v

--- ChargeCore/src/ChargeCore/Open/Open.v
+++ ChargeCore/src/ChargeCore/Open/Open.v
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 Require Import ChargeCore.Open.Stack.
 
-=======
-From ChargeCore.Open Require Import Stack.
->>>>>>> 4654b4d07959ce9066068d583192d3b88f2f8afe
 Require Import List FunctionalExtensionality.
 Require Import Coq.Classes.RelationClasses.
 
@@ -21,11 +17,7 @@ Section Expr.
 
   Polymorphic Definition open B : Type := stack A val -> B.
 
-<<<<<<< HEAD
   Polymorphic Program Definition lift {A B} (f : A -> B) (a : open A) : open B := 
-=======
-  Program Definition lift {A B} (f : A -> B) (a : open A) : open B :=
->>>>>>> 4654b4d07959ce9066068d583192d3b88f2f8afe
     fun x => f (a x).
 
   Polymorphic Definition expr := open val.
@@ -35,7 +27,6 @@ Section Expr.
   Proof.
     split; intuition congruence.
   Qed.
-<<<<<<< HEAD
   
   Polymorphic Definition open_const {B : Type} (b : B) : open B := fun s => b.
   
@@ -44,16 +35,6 @@ Section Expr.
   Polymorphic Definition empty_open : expr := fun x => null.
 
   Polymorphic Definition uncurry {A B C} (f : A -> B -> C) : (A * B) -> C := 
-=======
-
-  Definition open_const {B : Type} (b : B) : open B := fun s => b.
-
-  Definition V_expr (v : val) : expr := fun s => v.
-  Definition var_expr (x : A) : expr := fun s => s x.
-  Definition empty_open : expr := fun x => null.
-
-  Definition uncurry {A B C} (f : A -> B -> C) : (A * B) -> C :=
->>>>>>> 4654b4d07959ce9066068d583192d3b88f2f8afe
     fun x => f (fst x) (snd x).
   Polymorphic Definition curry {A B C} (f : A * B -> C) : A -> B -> C :=
     fun x y => f (x, y).
@@ -66,7 +47,7 @@ Section Expr.
       | cons T Ts => (T * Tprod Ts)%type
     end.
 
-  Fixpoint my_arrows (Ts : list Type) (R : Type) : Type :=
+  Fixpoint my_arrows (Ts : list Type) (R : Type) : Type := 
     match Ts with
       | nil => R
       | T :: Ts => T -> my_arrows Ts R
@@ -81,7 +62,7 @@ Section Expr.
     simpl. split. reflexivity.
   Defined.
 
-  Global Instance MyEqCons {A B Ts R} (H : MyEq B (my_arrows Ts R)) :
+  Global Instance MyEqCons {A B Ts R} (H : MyEq B (my_arrows Ts R)) : 
     MyEq (A -> B) (my_arrows (A::Ts) R) | 10.
   Proof.
     split. simpl.
@@ -98,7 +79,7 @@ Section Expr.
 
   Fixpoint uncurry_open Ts R :
     (open (Tprod Ts) -> open R) -> my_arrows (map open Ts) (@open R) :=
-    match Ts with
+    match Ts with 
       | nil => fun f => f (fun x => tt)
       | T :: Ts =>  fun f => fun x => uncurry_open (fun y => f (opair x y))
     end.
